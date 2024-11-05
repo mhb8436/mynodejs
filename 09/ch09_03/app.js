9680ffb91a357c9442262ef31c4bec6a6b8f7e76
mhb8436 <mhb8436@gmail.com>
2024-11-05 10:46:56 +0900

    aa

--- 09/ch09_03/app.js
+++ 09/ch09_03/app.js
@@ -38,6 +38,53 @@ app.post("/posts", async (req, res) => {
         res.status(500).json({message: e});
     }
 });
+// post list find
+app.get("/posts", async (req, res)=> {
+    try{
+        const posts = await Post.find({});
+        res.status(200).json({data: posts, message:'ok'});
+    }catch(e){
+        res.status(500).json({message: e});
+    }
+});
+
+app.get("/posts/:id", async (req, res)=> {
+    const { id } = req.params; // _id: 672970a85cf76dff95bdd4f5 GET => localhost:3000/posts/672970a85cf76dff95bdd4f5
+    try{
+        const post = await Post.findById(id);
+        res.status(200).json({data: post, message:'ok'});
+    }catch(e){
+        res.status(500).json({message: e});
+    }
+});
+
+app.put("/posts/:id", async (req, res)=> {
+    const { id } = req.params;
+    const { title, content } = req.body; // json 
+    try{
+        const post = await Post.findByIdAndUpdate(
+            id,
+            {
+                title: title, 
+                content: content,
+            },
+            {new : true} // 업데이트가 적용된 후의 문서를 반환 합니다. 
+        )
+        res.status(200).json({data: post, message:'ok'});
+    }catch(e){
+        res.status(500).json({message: e});
+    }
+});
+
+app.delete("/posts/:id", async (req, res) => {
+    const {id } = req.params;
+    try{
+        await Post.findByIdAndDelete(id);
+        res.status(204).send();
+    }catch(e){
+        res.status(500).json({message: e});
+    }
+});
 
 app.listen(3000, ()=>{
     console.log(`server is running`);
